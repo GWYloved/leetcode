ec5a9cd32347c36e5af089169e5b697e1c376ea2
GWYloved <913925047@qq.com>
2018-12-24 17:31:18 +0800

    optimistics/改写脚本

---
+++ rewrite.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="yinpengcheng"
+CORRECT_NAME="GWYloved"
+CORRECT_EMAIL="913925047@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
