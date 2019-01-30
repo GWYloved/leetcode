45f236be3426c027d80a33855a94f6060feab954
yinpengcheng <913925047@qq.com>
2019-01-30 14:30:44 +0800

    feature/solution to 169

--- src/array/array_169_majority_element.java
+++ src/array/array_169_majority_element.java
@@ -1,6 +1,8 @@
 package array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
@@ -22,12 +24,11 @@ class array_169_majority_element {
             return nums[0];
         }
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        int former = 1, last = 1;
+        int former,last;
         if (nums[0] == nums[1]){
             former = 1; last = 1;
         }else {
-            former = 0; last = 0;
+            former = 1; last = 0;
         }
         int flag = nums[0];
         int big = nums[0];
@@ -38,12 +39,11 @@ class array_169_majority_element {
                     big = nums[i];
                     former = last;
                 }
+                if (last > nums.length /2){
+                    return big;
+                }
             }else {
                 flag = nums[i];
-//                if (last > former){
-//                    big = nums[i-1];
-//                    former = last;
-//                }
                 last = 1;
             }
         }
@@ -51,7 +51,32 @@ class array_169_majority_element {
     }
 
     public static void main(String[] args) {
-        System.out.println(majorityElement(new int[]{2,2,1,1,1,2,2}));
+        System.out.println(majorityElement(new int[]{6,5,5}));
     }
 
+    /**
+     * 思路：这题主要是计算最大的数字的数量，先排序，在遍历，这样时间复杂度最多为O(nlogn)，不过有个更好的方法
+     * 最佳实践：
+     * 直接一遍遍历，有一个数字做标记位，存储一个数，当当前这个数和存储的数相等时，标记位加1，否则减1，
+     * 当标记位为0的时候，替换存储数，最后存储数就是最大的数。
+     * 核心思想在于，有一个不同的，之前存的最大的数就应该减少一个，但是题目说必有一个数量大于N/2，所以就肯定会有一个数字的数量
+     * 减去其余所有数字的数量结果仍然比0大的。
+     * 这个方法有点取巧，如果是N/3的话，那就不行了。那就得老老实实排序之后处理了。
+     */
+
+    public static int majorityElement1(int[] nums) {
+        int flag = 0;
+        int can = nums[0];
+        for (int i : nums){
+            if (flag == 0){
+                can = i;
+            }
+            if (can == i){
+                flag ++;
+            }else {
+                flag --;
+            }
+        }
+        return can;
+    }
 }
