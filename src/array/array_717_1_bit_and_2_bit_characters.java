367ef54e1fb21e71e908a8eea414ce4c47279ba9
yinpengcheng <913925047@qq.com>
2019-03-01 17:33:58 +0800

    solution/solution to 717

--- src/array/array_717_1_bit_and_2_bit_characters.java
+++ src/array/array_717_1_bit_and_2_bit_characters.java
@@ -23,7 +23,25 @@ Note:
 bits[i] is always 0 or 1.
  */
 class array_717_1_bit_and_2_bit_characters {
-    public boolean isOneBitCharacter(int[] bits) {
-
+    public static boolean isOneBitCharacter(int[] bits) {
+        int length = bits.length;
+        for (int i = 0; i < length; i++) {
+            if (bits[i] == 1) {
+                i++;
+                continue;
+            }
+            if (i == length - 1) {
+                return bits[i] == 0;
+            }
+        }
+        return false;
     }
+
+    public static void main(String[] args) {
+        System.out.println(isOneBitCharacter(new int[]{1, 0, 1, 0}));
+    }
+
+    /**
+     * 思路：这题略无聊，主要是指针的移动，如果当前数字是1，往后挪一位在++，如果当前是0，直接++，最后判断指针是否落在最后一位即可。
+     */
 }
