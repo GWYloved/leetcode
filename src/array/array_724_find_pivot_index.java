5023ba24e5443473f4be17a640131077270a702d
GWYloved <913925047@qq.com>
2019-03-03 21:26:49 +0800

    feature/solution to 724

--- src/array/array_724_find_pivot_index.java
+++ src/array/array_724_find_pivot_index.java
@@ -33,6 +33,29 @@ Each element nums[i] will be an integer in the range [-1000, 1000].
  */
 class array_724_find_pivot_index {
     public int pivotIndex(int[] nums) {
-
+        if (nums.length < 1){
+            return -1;
+        }
+        int totalSum = 0;
+        for (int i : nums){
+            totalSum += i;
+        }
+        if (totalSum == nums[0]){
+            return 0;
+        }
+        int flagSum = 0;
+        for (int i = 0; i < nums.length -1; i ++){
+            flagSum += nums[i];
+            totalSum -= nums[i];
+            if (totalSum - nums[i+1] == flagSum){
+                return i +1;
+            }
+        }
+        return -1;
     }
+    /**
+     * 解题思路：这一题是求平衡点，刚开始我觉得双指针前后挪动可以，但是后来发现可以有负数的。
+     * 之后思路做了一个转变，就是先O(n)复杂度求个和，然后单指针挪动求前部分的和。
+     * 不过又一个wrong answer，就是可以返回0，因为整个和加起来等于第0个，这样返回0就好了。
+     */
 }
