a3005ab186c147e9f375200ff81145ae5dcb86bc
YinPengCheng <mikilangkilo.yin@ximalaya.com>
2020-03-15 18:34:21 +0800

    feature/solution to 852

---
+++ src/binary_search/bs_349_intersection_of_two_arrays.java
@@ -0,0 +1,31 @@
+package binary_search;
+
+import utils.ListUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given two arrays, write a function to compute their intersection.
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * Example 2:
+ *
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Note:
+ *
+ * Each element in the result must be unique.
+ * The result can be in any order.
+ */
+public class bs_349_intersection_of_two_arrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[] big = nums1.length > nums2.length? nums1 : nums2;
+        int[] small = nums1.length < nums2.length ? nums2 : nums1;
+        int[] output = new int[small.length];
+    }
+}
