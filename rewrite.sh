6e8e7bea160d3d5be99c5416375212d7c6391ce3
YinPengCheng <mikilangkilo.yin@ximalaya.com>
2020-04-16 16:47:35 +0800

    feature/change script

--- rewrite.sh
+++ rewrite.sh
@@ -2,6 +2,7 @@
 
 git filter-branch --env-filter '
 OLD_EMAIL="gwyloved@gmail.com"
+OLD_NAME="yinpengcheng"
 CORRECT_NAME="GWYloved"
 CORRECT_EMAIL="913925047@qq.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
@@ -10,6 +11,11 @@ then
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
