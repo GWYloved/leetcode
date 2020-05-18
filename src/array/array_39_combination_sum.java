3c6e24944e6b7d3613478d82f80db5c7ce6833d4
YinPengCheng <mikilangkilo.yin@ximalaya.com>
2020-05-18 20:16:30 +0800

    feature/solution to 39

--- src/array/array_39_combination_sum.java
+++ src/array/array_39_combination_sum.java
@@ -1,5 +1,8 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 /*
@@ -30,7 +33,33 @@ A solution set is:
 ]
  */
 class array_39_combination_sum {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        return null;
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        List<Integer> tempList = new ArrayList<>();
+        fun(result,tempList, candidates,target,0);
+        return result;
     }
+    private static void fun(List<List<Integer>> result,List<Integer> temp, int[] candidates, int remain, int start){
+        for (int i = start; i < candidates.length; i ++){
+            int tempRemain = remain - candidates[i];
+            List<Integer> list = new ArrayList<>(temp);
+            if (tempRemain > 0){
+                list.add(candidates[i]);
+                fun(result, list, candidates, tempRemain, i);
+            }else if (tempRemain == 0){
+                list.add(candidates[i]);
+                result.add(list);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(combinationSum(new int[]{2,3,5},8));
+    }
+
+    /**
+     * 解题思路：每次都可以以自身为单位往后回溯
+     * 比较坑的是需要注意每次回溯的时候需要新建一个容器进行数据拷贝
+     */
 }
