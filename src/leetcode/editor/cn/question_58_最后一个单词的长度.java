6502e22cb0846d397ae3f194f13b353497992130
yinpengcheng <913925047@qq.com>
2020-08-05 00:28:20 +0800

    feature/add leetcode plugin

---
+++ src/leetcode/editor/cn/question_58_最后一个单词的长度.java
@@ -0,0 +1,37 @@
+//Given a string s consists of upper/lower-case alphabets and empty space charac
+//ters ' ', return the length of last word (last word means the last appearing wor
+//d if we loop from left to right) in the string. 
+//
+// If the last word does not exist, return 0. 
+//
+// Note: A word is defined as a maximal substring consisting of non-space charac
+//ters only. 
+//
+// Example: 
+//
+// 
+//Input: "Hello World"
+//Output: 5
+// 
+//
+// 
+// Related Topics 字符串 
+// 👍 226 👎 0
+
+
+package leetcode.editor.cn;
+//Java：最后一个单词的长度
+public class question_58_最后一个单词的长度{
+    public static void main(String[] args) {
+        Solution solution = new question_58_最后一个单词的长度().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLastWord(String s) {
+        return 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
